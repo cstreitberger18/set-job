111bf0b3b4f7453c72c4f4b9336660b04f405068
Christoph Streitberger <cstreitberger@schueler.hakzell.at>
2024-06-04 17:24:33 +0200

    klasse job hinzufügen

---
+++ src/Job.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Cstreitberger18\SetJob;
+
+class Job
+{
+    private $jobs = [
+        "Mechaniker",
+        "Lehrer",
+        "Schlosser",
+        "Tischler",
+        "Rennfahrer",
+        "Bäcker",
+        "Musiker",
+        "Schauspieler"
+    ];
+
+    public function getRandomJob()
+    {
+        return $this->jobs[array_rand($this->jobs,1)];
+    }
+}
